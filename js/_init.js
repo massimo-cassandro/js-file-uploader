c043358521005ca1d6b6807fff2e70c60173f3b7
Massimo Cassandro <massimo.cassandro@blu-net.it>
2019-06-03 18:38:18 +0200

    sviluppo nuova release

--- js/_init.js
+++ js/_init.js
@@ -1,311 +1,99 @@
-/* globals FileUploader:true */
+/* exported FileUploader */
 
-FileUploader = ((upl) => {
-  "use strict";
+var FileUploader = {
 
-  /**
-	 * isSuitableBrowser
-	 * Verifica che il browser sia in grado di gestire le funzionalità richieste.
-	 *
-   * - Restituisce `true` o `false`
-	 */
-	const isSuitableBrowser = () => {
-		var div = document.createElement( 'div' );
-		return ( ( 'draggable' in div ) || ( 'ondragstart' in div && 'ondrop' in div ) )
-      && 'FormData' in window
-      && 'FileReader' in window;
-	},
+  // VARIABILI GLOBALI
+  /*
+    mimetypes ed estensioni da utilizzare per l'attributo `accept` del
+    tag input in corrispondenza del parametro `filetype`
+  */
+  mimetypes: {
+    img: ['image/png', 'image/jpeg', 'image/pjpeg', 'image/gif', 'image/webp',
+          '.png', '.jpg', '.jpeg', '.pjpg', '.pjpeg', '.gif', '.webp'],
 
-  /**
-	 * parse_max_filesize
-	 * elabora il parametro max_filesize (se esiste) e restituisce:
-	 *   - `null` se `max_filesize === null`
-	 *   - `false` se `max_filesize` non è un valore corretto
-	 *   - l'oggetto `{ maxbytes: 123456, unit: 'KB', feedback_size: ''}`
-	 *   in cui:
-	 *     - `maxbytes` è il valore in bytes del limite imposto
-	 *     - `unit` è uno tra 'KB' e 'MB'
-	 *.    - `feedback_size` è la rappresentazione per un eventuale feedback per l'utente
-	 */
-  parse_max_filesize = (upl_options) =>  {
-    // controllo max_filesize ed elaborazione parametro
-    if( upl_options.max_filesize ) {
-      var maxbytes, unit, feedback_size;
+    pdf: ['application/pdf', '.pdf']
+  },
 
-      if(!isNaN(upl_options.max_filesize)) { //KB
-        maxbytes = +upl_options.max_filesize;
-        unit = 'KB';
-        feedback_size = upl_options.max_filesize;
+  // parametri di default condivisi da tutte le istanze
+  default_global_options: {
 
-        // se l'unità è indicata in KB ma il valore è più grande
-        // di un MB, il parametro viene aggiornato
-        if( maxbytes >= 1024 ) {
-          unit = 'MB';
-          feedback_size = (Math.round((maxbytes / 1024) * 100) /100);
-        }
+    // percorso del css FileUploader
+    fileUploader_css: null,
 
-      } else {
+    // Funzione richiamata ogni volta che FileUploader viene inizializzato
+    init_callback: null,
 
-        unit = upl_options.max_filesize.substr(-2, 2).toUpperCase();
-        maxbytes = +upl_options.max_filesize.substr(0, upl_options.max_filesize.length-2);
-        feedback_size = maxbytes;
+    // Selettore degli elementi su cui applicare FileUploader.
+    selector: '[data-file_uploader]',
 
-        if(isNaN(maxbytes) || (unit !== 'KB' && unit !== 'MB') ) {
-          return false;
-        }
-      }
+    // Attiva  la modalità debug che mostra in console le informazioni sulla
+    debug: false,
 
-      if(unit === 'KB') {
-        maxbytes = maxbytes * 1024;
-      } else {
-        maxbytes = maxbytes * 1024 * 1024;
-      }
+    // Interfaccia per l'invio di messaggi di errore
+    alert_api: (mes) => { window.alert(mes);}
+  },
 
-      return {
-        'originalValue': upl_options.max_filesize,
-        'maxbytes'     : maxbytes,
-        'unit'         : unit,
-        'feedback_size': String(feedback_size).replace(/\./, ',') + ' ' + unit
-      };
+  // parametri personalizzabili per ogni singola istanza
+  default_istance_options: {
 
-    } else {
-      return null;
-    }
-  };
+    // Url dello script lato server che esegue il caricamento del file
+    uploader_url: null,
 
+    // Tipologia dei file selezionabili
+    filetype: 'auto',
 
-  //!init
-  /**
-	 * init
-	 * Seleziona gli elementi con l'attributo upl.global_options.selector e avvia FileUploader
-	 */
-	upl.init = (user_global_options, user_istance_options) => {
+      // Eventuale classe da aggiungere all'elemento contenitore di FileUploader
+    container_class: null,
 
-    if( !isSuitableBrowser() ) {
-      console.error( "Questo browser non ha tutte le funzionalità richieste da FileUploader" );// eslint-disable-line
-      return;
-    }
+    // Template del markup da inserire all'interno dell'elemento contenitore
+    template: '<div class="card text-center" style="background-color: transparent">' +
+        '<div class="card-body">' +
+          '<div class="file_upl__button"></div>' +
+          '<div class="file_upl__dd_text"></div>' +
+        '</div>' +
+        '<div class="card-footer text-muted small font-italic file_upl__info_text"></div>' +
+      '</div>',
 
-    // impostazioen parametri globali
-    if( typeof Object.assign === "function") {
-      upl.global_options = Object.assign({}, upl.global_options, user_global_options);
-    } else { // IE
-      for(let i in upl.global_options) {
-        if( user_global_options[i] ) {
-          upl.global_options[i] = user_global_options[i];
-        }
-      }
-    }
+    //Funzione richiamata ogni volta che un file viene inviato al server.
+    upload_complete_callback: null,
 
-    // caricamento CSS
-    if(upl.global_options.fileUploader_css) {
-      document.head.insertAdjacentHTML('beforeend',
-        '<link rel="stylesheet" href="' + upl.global_options.fileUploader_css + '" type="text/css" media="all">'
-      );
-    }
+    // Eventuale classe da aggiungere all'elemento  contenitore quando un file vi
+    container_dragover_class: 'bg-light',
 
-    document.querySelectorAll(upl.global_options.selector).forEach( (upl_el) => {
+    // Eventuale testo da sostituire a quello contenuto nel tag label
+    label_text: null,
 
-      // upl_el → elemento corrente su cui è istanziato FileUploader
+    // Eventuale classe da applicare al tag label
+    label_class: 'btn btn-primary btn-lg',
 
-      let this_upl = new upl.uploader();
+    // Testo di istruzioni per il drag and drop con input senza attributo `multiple`
+    dd_text_single: 'oppure trascina qui un file',
 
-      /*
-        merge dei parametri inseriti tramite attributi `data`, in cui:
+    // Testo di istruzioni per il drag and drop  con input `multiple`
+    dd_text_multiple: 'oppure trascina qui uno o più file',
 
-        * `upl_el.dataset`:
-            tutti gli attributi inseriti singolarmente (es. data-filetype="img")
+    // Indica se mostrare o no il testo informativo su formati accettati
+    show_info_text: true,
 
-        * `upl_el.dataset.file_uploader`:
-            attributi inseriti tramite json assegnato a `data-fileuploader
-            (es data-fileuploader='{"filetype":"img"}')
+    // Eventuale testo informativo personalizzato. se presente, sostituisce il
+    custom_info_text: null,
 
-        In caso di conflitto prevalgono gli ultimi
-      */
+    // Stringa utilizzata per concatenare tra loro le varie parti del testo informativo
+    info_text_join_string: '<br>',
 
-      let file_uploader_data = upl_el.dataset.file_uploader;
-      if(file_uploader_data === '') {
-        file_uploader_data = {};
-      }
+    // impostazioni per le immagini. Applicate solo se `filetype == 'img'`.
+    img_min_w   : null,
+    img_max_w   : null,
+    img_w       : null,
+    img_min_h   : null,
+    img_max_h   : null,
+    img_h       : null,
 
-      if( typeof Object.assign === "function") {
+    // Dimensione (peso) massima dell'immagine.
+    max_filesize: null,
 
-        this_upl.istance_options = Object.assign({},
-          upl.default_istance_options,
-          user_istance_options,
-          upl_el.dataset,
-          file_uploader_data
-        );
+    // Prefisso della variabile utilizzata per inviare al server i dati
+    varname: 'file'
+  }
 
-      } else { // IE
-
-        for(let i in upl.default_istance_options) {
-          if( upl_el.dataset.file_uploader[i] ) {
-            this_upl.istance_options[i] = upl_el.dataset.file_uploader[i];
-          } else if( upl_el.dataset[i] ) {
-            this_upl.istance_options[i] = upl_el.dataset[i];
-          } else if( user_istance_options[i] ) {
-            this_upl.istance_options[i] = user_istance_options[i];
-          } else {
-            this_upl.istance_options[i] = upl.default_istance_options[i];
-          }
-        }
-      }
-
-      // cancella la chiave `file_uploader` al solo scopo di ridurre la confusione
-      delete this_upl.istance_options.file_uploader;
-
-      // aggiunta dell'elemento stesso ad  `istance_options`:
-      this_upl.istance_options.element = upl_el;
-
-      // ottimizzazione dei parametri
-      this_upl.istance_options.filetype = this_upl.istance_options.filetype.toLowerCase();
-
-      //debug
-      if( upl.global_options.debug ) {
-        /* eslint-disable */
-        console.groupCollapsed('FileUploader global & istance options');
-        console.table(upl.global_options);
-        if( window.navigator.userAgent.toLowerCase().indexOf('chrome') === -1 ) {
-          console.table(Object.assign({}, this_upl.istance_options));
-        }
-        console.log(this_upl.istance_options);
-        console.groupEnd();
-        /* eslint-enable */
-      }
-
-      try {
-        // !controllo parametri obbligatori
-        if( !this_upl.istance_options.uploader_url ) {
-          throw new Error( "`uploader_url` non impostato" );
-        }
-        if( !this_upl.istance_options.upload_start_callback ) {
-          throw new Error( "`upload_start_callback` non impostato" );
-        }
-        if( !this_upl.istance_options.upload_complete_callback ) {
-          throw new Error( "`upload_complete_callback` non impostato" );
-        }
-        var max_filesize = parse_max_filesize(this_upl.istance_options);
-        if( max_filesize === false ) {
-          throw new Error( '"' + this_upl.istance_options.max_filesize + '" non è un valore corretto per `max_filesize`');
-        } else {
-          this_upl.istance_options.max_filesize = max_filesize;
-        }
-
-        //         //!SETUP
-//         // aggiunta riferimento container ad upl.istance_options
-//         this_upl.istance_options.container = upl_el;
-
-//         // aggiunta della classe per l'attivazione delle regole css
-//         this_upl.istance_options.container.addClass('fileupl');
-//         // aggiunta classe personale
-//         if( this_upl.istance_options.container_class ) {
-//           this_upl.istance_options.container.addClass( this_upl.istance_options.container_class );
-//         }
-
-//         // rilevamento impostazioni ed elementi (prima di init_callback)
-//         var _label = this_upl.istance_options.container.find('label'),
-//           _input = this_upl.istance_options.container.find('input:file');
-
-//         this_upl.istance_options.is_multiple = _input.prop('multiple');
-
-//         // esecuzione init_callback, se presente
-//         if( this_upl.istance_options.init_callback !== null ) {
-//           this_upl.istance_options.init_callback( this_upl.istance_options );
-//         }
-
-//         // aggiunta template
-//         var original_html = this_upl.istance_options.container.html();
-//         this_upl.istance_options.container.html( this_upl.istance_options.template );
-
-//         this_upl.istance_options.container.find('.fileupl__button').html( original_html ); //ripristino markup originale
-
-
-
-//         // rilevamento attributo name e sua eliminazione ad evitare che sia inviato con il form
-//         this_upl.istance_options.input_name = _input.attr('name') || 'uploader_file[]';
-//         _input.removeAttr('name');
-
-//         // impostazione parametri accept
-//         if( this_upl.istance_options.filetype === 'auto' ) {
-//           this_upl.istance_options.accept = []; // raccolta di upl.mimetypes ed estensioni per controllo sul drag&drop
-//           if( _input.attr('accept')) {
-//             this_upl.istance_options.accept = _input.attr('accept').split(',').map(function (item) { return item.trim(); });
-//           }
-
-//         } else if( Object.keys(upl.mimetypes).indexOf( this_upl.istance_options.filetype ) !== -1) { // solo i tipi mappati
-
-//           this_upl.istance_options.accept = upl.mimetypes[this_upl.istance_options.filetype].slice(0); // slice → clona
-//           _input.attr( 'accept', this_upl.istance_options.accept.join(',') );
-
-//         } else { // filetype non corretto
-//           throw new Error( "`filetype` non corretto" );
-//         }
-
-//         // se esiste this_upl.istance_options.label_text, il testo del tag label viene sostituito
-//         if( this_upl.istance_options.label_text ) {
-//           _label.text( this_upl.istance_options.label_text );
-//         }
-
-//         // se esiste this_upl.istance_options.label_class, la classe viene aggiunta
-//         if( this_upl.istance_options.label_class ) {
-//           _label.addClass( this_upl.istance_options.label_class  );
-//         }
-
-//         // aggiunta del testo info per il drag & drop
-//         this_upl.istance_options.container.find('.fileupl__dd_text').html( this_upl.istance_options.is_multiple ? this_upl.istance_options.dd_text_multiple : this_upl.istance_options.dd_text_single );
-
-//         // aggiunta testo info file
-//         this_upl.istance_options.container.find('.fileupl__info_text')
-//         .html(
-//           (this_upl.istance_options.show_info_text ? upl.create_info_text(this_upl.istance_options) : '') +
-//           ((this_upl.istance_options.show_info_text && this_upl.istance_options.custom_info_text)? '<br>' : '') +
-//           (this_upl.istance_options.custom_info_text ? this_upl.istance_options.custom_info_text : '')
-//         );
-
-
-//         // !DRAG&DROP
-//         this_upl.istance_options.container
-//         .on( 'drag dragstart dragend dragover dragenter dragleave drop', function( e ) {
-//           e.preventDefault();
-//           e.stopPropagation();
-//         })
-//         .on( 'dragover dragenter', function() {
-//           this_upl.istance_options.container.addClass( this_upl.istance_options.container_dragover_class );
-//         })
-//         .on( 'dragleave dragend drop', function() {
-//           this_upl.istance_options.container.removeClass( this_upl.istance_options.container_dragover_class );
-//         })
-//         .on( 'drop', function( e ) {
-//           var err = false;
-//           if( !this_upl.istance_options.is_multiple && e.originalEvent.dataTransfer.files.length > 1 ) {
-
-//             this_upl.istance_options.alertErrorAPI('Puoi selezionare un solo file!');
-//             err = true;
-//           }
-
-//           if(!err) {
-//             upl.sendFiles( e.originalEvent.dataTransfer.files, this_upl.istance_options ); // filelist
-//           }
-//         });
-
-// //TODO fumoso, da riscrivere
-// //TODO correggere selector, possibilità non abbia id
-//         // !selezione tramite input
-//         var input_selector = '#' + _input.attr('id');
-//         $(document).on('change', input_selector, function () {
-//           upl.sendFiles( document.querySelector(input_selector).files, this_upl.istance_options ); // filelist
-//         });
-
-
-      } catch(e) { //throw "error"
-        console.error( e );// eslint-disable-line
-      } // finally {}
-
-    }); // end document.querySelectorAll(upl.global_options.selector).forEach
-
-	}; // end init
-
-  return upl;
-
-})(FileUploader || {});
+};
--- js/file_uploader.js
+++ js/file_uploader.js
@@ -1,54 +1,22 @@
-/* eslint no-console: 0, no-unused-vars: 0, no-empty:  0 */
-/* exported FileUploader */
+/*!
+ *
+ * FileUploader 2.0
+ * HTML5 / JS Async Uploader
+ *
+ */
 
-// FileUploader 2.0.0
+/* eslint no-console: 0, no-unused-vars: 0, no-empty:  0 */
+/* globals FileUploader */
 
 /*
+  @codekit-prepend '_init.js'
 
-  @codekit-append '_defaults.js';
-  -codekit-append '_create_info_text.js';
-  -codekit-append '_build_hidden_fields.js';
-  -codekit-append '_sendFiles.js';
-  @codekit-append '_init.js';
 */
 
-var FileUploader = (() => {
+
+(() => {
   "use strict";
 
-  // VARIABILI GLOBALI
-  let upl = {
 
-    /*
-      opzioni risultanti dal merge di `default_global_options` e da
-      quelle inviate all'inizializzazione di FileUploader.
-      Il default è impostato in `_default.js`, il valore definitivo
-      è impostato in `_init.js`
-    */
-    global_options: {},
 
-    /*
-  	 mimetypes ed estensioni da utilizzare per l'attributo `accept` del
-  	 tag input in corrispondenza del parametro `filetype`
-  	*/
-  	mimetypes: {
-    	img: ['image/png', 'image/jpeg', 'image/pjpeg', 'image/gif', 'image/webp',
-      	    '.png', '.jpg', '.jpeg', '.pjpg', '.pjpeg', '.gif', '.webp'],
-
-    	pdf: ['application/pdf', '.pdf']
-    },
-
-    // classe uploader
-    uploader: function () {
-      /*
-        opzioni risultanti dal merge di `default_options`, da quelle inviate
-        all'inizializzazione di FileUploader e da quelle associate all'elemento
-        FileUploader tramite attributi data.
-        Il default è impostato in `_default.js`, il valore definitivo
-        è impostato in `_init.js`
-      */
-      this.istance_options = {};
-    }
-  };
-
-  return upl;
 })();
