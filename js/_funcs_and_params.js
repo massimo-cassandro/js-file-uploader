a0c1dde608a7a0b1f6bcd750b21f38985d8bb59e
mazz2 <>
2019-12-30 16:10:32 +0100

    file size parsing fix

--- js/_funcs_and_params.js
+++ js/_funcs_and_params.js
@@ -35,8 +35,14 @@ FileUploader = ((upl) => {
     let mega = 1024*1024;
     if(bytes >= mega ) {
       return (bytes/mega).toLocaleString(locales, {maximumFractionDigits: 1}) + '<span class="fupl-unit">MB</span>';
+
+    } else if (bytes < 1024) {
+      return Number((bytes/1024).toFixed(2)).toLocaleString(locales, {maximumFractionDigits: 2}) +
+        '<span class="fupl-unit">KB</span>';
+
     } else {
-      return Math.round(bytes/1024).toLocaleString(locales, {maximumFractionDigits: 0}) + '<span class="fupl-unit">KB</span>';
+      return Math.round(bytes/1024).toLocaleString(locales, {maximumFractionDigits: 0}) +
+        '<span class="fupl-unit">KB</span>';
     }
   };
 
